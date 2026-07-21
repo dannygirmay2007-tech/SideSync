157da082f7c660820f9c5110c21e70b706b79c36
dannygirmay2007-tech <Dannygirmay2007@gmail.com>
2026-07-20 22:49:00 -0500

    Add dynamic homepage template

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "SideSync"
+    players = ["Danny", "Aden", "Yafiet", "Player 4"]
+    return render_template("index.html", players = players, player_count = len(players))
 if __name__ == "__main__":
     app.run(debug=True)
