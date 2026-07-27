684de3f26e5c3b014b2f8ae43e90d2f82467fc5b
dannygirmay2007-tech <Dannygirmay2007@gmail.com>
2026-07-27 11:25:00 -0500

    Add player submission form

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-@app.route("/")
+players = ["Danny", "Aden", "Yafiet", "Player 4"]
+@app.route("/", methods=["GET","POST"])
 def home():
-    players = ["Danny", "Aden", "Yafiet", "Player 4"]
+    if request.method == "POST":
+        player_name = request.form["player_name"].strip()
+
+        if player_name:
+            players.append(player_name)
+        return redirect(url_for("home"))
     return render_template("index.html", players = players, player_count = len(players))
 if __name__ == "__main__":
     app.run(debug=True)
